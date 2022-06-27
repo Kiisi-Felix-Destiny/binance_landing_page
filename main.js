1c10db5e455f470a6d007fe18a575cf4c281cb59
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-06-27 01:35:29 +0100

    Done

--- main.js
+++ main.js
@@ -1,91 +1,33 @@
 let themeColorBtn = document.querySelector(".b-header_theme")
 
-let body = document.querySelector("body")
-let headerSection = document.querySelector(".b-header")
-let getStartedSection = document.querySelector(".b-get_started")
-let statsSection = document.querySelector(".b-stats")
-let cryptocurrenciesSection = document.querySelector(".b-cryptocurrencies-wrapper")
-let signupBoxSection = document.querySelector(".b-signup_box")
-let cryptoPortfolioSection = document.querySelector(".b-crypto_portfolio")
-let exploreSection = document.querySelector(".b-explore")
-let trustedCryptoExchangeSection = document.querySelector(".b-trusted_crypto-exchange")
-let startEarningTodaySection = document.querySelector(".b-start-earning-today")
-
-let getStartedInput = document.querySelector(".b-get_started-input")
-
-
-
-
-
-
-
-let switchColorThemeText = document.querySelectorAll(".b-switch-color-theme")
-let switchColorThemeTextLight = document.querySelectorAll(".b-switch-color-theme-light")
 
 
 let darkmode = document.querySelector(".darkmode_lightmode");
 
-//IMG THEME
-let getStartedImg = document.querySelector(".b-get_started-img")
-
+/*
+'rgb(30, 35, 41)'
+rgb(11, 14, 17)
+'rgb(24, 26, 32)'
+*/
 
 themeColorBtn.addEventListener("click",()=>{
     if(darkmode.textContent == 'dark_mode'){
         darkmode.textContent = 'light_mode'
 
-        body.style.background = 'rgb(30, 35, 41)'
-        headerSection.style.background = 'rgb(24, 26, 32)'
-        getStartedSection.style.background = 'rgb(30, 35, 41)'
-        statsSection.style.background = 'rgb(30, 35, 41)'
-        cryptocurrenciesSection.style.background = 'rgb(24, 26, 32)'
-        signupBoxSection.style.background = 'rgb(24, 26, 32)'
-        cryptoPortfolioSection.style.background = 'rgb(24, 26, 32)'
-        exploreSection.style.background = 'rgb(24, 26, 32)'
-        trustedCryptoExchangeSection.style.background = 'rgb(24, 26, 32)'
-        startEarningTodaySection.style.background = 'rgb(11, 14, 17)'
-        getStartedInput.style.background = 'rgb(30, 35, 41)'
-
-        themeColorBtn.style.color = '#fff'
-        for (v of switchColorThemeText){
-            v.style.color = '#fff'
-        }
-        for (v of switchColorThemeTextLight){
-            v.style.color = 'rgb(183, 189, 198)';
-        }
-        //getStartedImg.style.background = `url("./images/foreground-image-dark.png")`;
-        getStartedImg.classList.add("b-get_started-dark")
-        
-
 
     }else{
         darkmode.textContent = 'dark_mode'
 
-        body.style.background = '#fff'
-        headerSection.style.background = '#fff'
-        getStartedSection.style.background = '#fff'
-        statsSection.style.background = '#fff'
-        cryptocurrenciesSection.style.background = '#fff'
-        signupBoxSection.style.background = '#fff'
-        cryptoPortfolioSection.style.background = '#fff'
-        exploreSection.style.background = '#fff'
-        trustedCryptoExchangeSection.style.background = '#fff'
-        startEarningTodaySection.style.background = '#fff'
-        getStartedInput.style.background = '#fff'
-
-
-        themeColorBtn.style.color = 'rgb(24, 26, 32)'
-        for (v of switchColorThemeText){
-            v.style.color = 'rgb(24, 26, 32)'
-        }
-        for (v of switchColorThemeTextLight){
-            v.style.color = '#474D57';
-        }
-
-        //getStartedImg.style.background = `url("./images/foreground-image-light.png")`
-        getStartedImg.classList.remove("b-get_started-dark")
 
     }
 })
+
+
+
+
+
+
+
 
 
 let menuBar = document.querySelector(".b-header_menu-bar");
@@ -109,3 +51,47 @@ modal.onclick = () =>{
     modal.classList.remove("show-hide") 
     document.body.style.overflowY = 'visible'
 }
+
+
+const toggleHeader = document.querySelectorAll(".content .header");
+const toggleBtn = document.querySelectorAll(".b-toggle");
+
+toggleHeader.forEach(toggle=>{
+    let x = toggle.children.item(1);
+    let detail = toggle.nextElementSibling;
+    toggle.addEventListener("click",()=>{
+        if (!detail.classList.contains('active')) {
+            detail.classList.add('active');
+            detail.style.height = 'auto';
+    
+            let height = detail.clientHeight + "px";
+    
+            detail.style.height = '0px';
+    
+            setTimeout(function () {
+                detail.style.height = height;
+            }, 0);
+        } else {
+            detail.style.height = '0px';
+    
+            detail.addEventListener('transitionend', function () {
+                detail.classList.remove('active');
+            }, {
+                once: true
+            });
+        }
+
+
+
+        if(x.textContent == 'add'){
+            x.textContent = "remove"
+            
+        }
+        else{
+            x.textContent = "add"
+        }
+
+    })
+})
+
+
